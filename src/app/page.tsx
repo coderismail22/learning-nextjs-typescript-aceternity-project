ef496428c9c242a7ce1f3b8e8a7350f49a68d423
ismail <coderismail22@gmail.com>
2024-03-16 21:16:00 +0600

    sticky-scroll-reveal-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
-import { SpotlightPreview } from "@/components/SpotLightPreview";
+import { SpotlightPreview } from "@/components/SpotlightPreview";
 import { CourseList } from "@/components/CourseList";
+import { StickyScrollRevealDemo } from "@/components/StickyScrollRevealDemo";
 
 export default function Home() {
   return (
     <main className="p-4">
       <SpotlightPreview />
       <CourseList />
+      <StickyScrollRevealDemo />
       {/* USE "INFINITE MOVING CARDS" */}
     </main>
   );
