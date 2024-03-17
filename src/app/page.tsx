af883a96a474a037aeccbcadbab26ca3b86ba573
ismail <coderismail22@gmail.com>
2024-03-17 12:42:50 +0600

    wavy-background-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <StickyScrollRevealDemo />
       <InfiniteMovingCardsDemo />
       <CardHoverEffectDemo />
-      {/* <WavyBackgroundDemo /> */}
+      <WavyBackgroundDemo />
     </main>
   );
 }
